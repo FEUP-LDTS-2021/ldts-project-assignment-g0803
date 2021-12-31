c3fb385b74ef5f7db0023bd7af555d6626de73c6
marineve17 <mariana.lobao2002@gmail.com>
2021-12-31 19:21:22 +0000

    feat: make controller tests pass

--- src/main/java/g0803/bindingofshiba/controller/game/MonsterController.java
+++ src/main/java/g0803/bindingofshiba/controller/game/MonsterController.java
@@ -3,13 +3,36 @@ package g0803.bindingofshiba.controller.game;
 import g0803.bindingofshiba.model.game.Monster;
 
 public class MonsterController {
+    private Monster monster;
 
-    public void moveToTarget(Monster monster, int x, int y) {
-
+    public MonsterController(Monster monster) {
+        this.monster = monster;
     }
 
-    public void takeDamage(Monster monster, int damage) {
+    private void moveToTarget(int x, int y) {
+        if (x < 0 || y < 0) {
+            throw new IllegalArgumentException("Invalid position");
+        }
 
+        if (x > monster.getX()) {
+            monster.setX(monster.getX() + 1);
+        } else if (x < monster.getX()) {
+            monster.setX(monster.getX() - 1);
+        }
+
+        if (y > monster.getY()) {
+            monster.setY(monster.getY() + 1);
+        } else if (y < monster.getY()) {
+            monster.setY(monster.getY() - 1);
+        }
+    }
+
+    private void takeDamage(int damage) {
+        this.monster.decreaseHpByAmount(damage);
+    }
+
+    public void tick(int x, int y) {
+        moveToTarget(x, y);
     }
 
 }
