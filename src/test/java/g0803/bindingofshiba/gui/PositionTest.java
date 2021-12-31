08b15bb5802de67bca3d677923b989cc64f6f6e2
Guilherme Almeida <gui1612@sapo.pt>
2021-12-31 15:42:38 +0000

    test: changes position tests

--- src/test/java/g0803/bindingofshiba/gui/PositionTest.java
+++ src/test/java/g0803/bindingofshiba/gui/PositionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class PositionTest {
@@ -15,42 +16,64 @@ public class PositionTest {
     @Test
     public void getLeft() {
         Position position = getPosition();
-        Assertions.assertEquals(new Position(3, 5), position.getDown());
+        Position left = position.getLeft();
+
+        Assertions.assertEquals(3, left.getX());
+        Assertions.assertEquals(5, left.getY());
     }
 
     @Test
     public void getRight() {
         Position position = getPosition();
-        Assertions.assertEquals(new Position(5, 5), position.getDown());
+        Position right = position.getRight();
+
+        Assertions.assertEquals(5, right.getX());
+        Assertions.assertEquals(5, right.getY());
     }
 
     @Test
     public void getUp() {
         Position position = getPosition();
-        Assertions.assertEquals(new Position(4, 4), position.getDown());
+        Position up = position.getUp();
+
+        Assertions.assertEquals(4, up.getX());
+        Assertions.assertEquals(4, up.getY());
     }
 
     @Test
     public void getDown() {
         Position position = getPosition();
-        Assertions.assertEquals(new Position(4, 6), position.getDown());
+        Position down = position.getDown();
+
+        Assertions.assertEquals(4, down.getX());
+        Assertions.assertEquals(6, down.getY());
     }
 
     @Test
     public void getNeighbours() {
         Position position = getPosition();
+        List<Position> neighbours = position.getNeighbours();
         List<Position> expected = new ArrayList<Position>(
                 Arrays.asList(
-                    new Position(2, 4),
                     new Position(3, 4),
                     new Position(4, 4),
-                    new Position(2, 5),
-                    new Position(4, 5),
-                    new Position(2, 6),
+                    new Position(5, 4),
+                    new Position(3, 5),
+                    new Position(5, 5),
                     new Position(3, 6),
-                    new Position(4, 6)
+                    new Position(4, 6),
+                    new Position(5, 6)
                 ));
 
-        Assertions.assertEquals(expected, position.getNeighbours());
+
+        boolean equal = true;
+
+
+        for (Position pos : expected) {
+            if (!neighbours.contains(pos))
+                equal = false;
+        }
+
+        Assertions.assertTrue(equal);
     };
 }
