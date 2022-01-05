ce6030b52d3e225c71abced273038e7e86c5a397
Guilherme Almeida <gui1612@sapo.pt>
2022-01-05 00:15:20 +0000

    feat: add PlayerController interfaces

--- src/main/java/g0803/bindingofshiba/controller/game/PlayerController.java
+++ src/main/java/g0803/bindingofshiba/controller/game/PlayerController.java
@@ -1,4 +1,23 @@
 package g0803.bindingofshiba.controller.game;
 
+import g0803.bindingofshiba.gui.keyboard.Keyboard;
+import g0803.bindingofshiba.model.game.Position;
+import g0803.bindingofshiba.model.game.elements.Player;
+
 public class PlayerController {
+    private Player player;
+    private Keyboard keyboard;
+
+    public PlayerController(Player player, Keyboard keyboard) {
+        this.player = player;
+        this.keyboard = keyboard;
+    }
+
+    public void movePlayer() {
+
+    }
+
+    public Position getNextPosition() {
+        return new Position(0, 0);
+    }
 }
