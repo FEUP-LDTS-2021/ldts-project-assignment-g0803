1552347011fdeff5639445304c8d5aac3e911abc
Guilherme Almeida <gui1612@sapo.pt>
2022-01-05 21:28:11 +0000

    feat: add PlayerView interfaces

--- src/main/java/g0803/bindingofshiba/view/game/PlayerView.java
+++ src/main/java/g0803/bindingofshiba/view/game/PlayerView.java
@@ -1,4 +1,17 @@
 package g0803.bindingofshiba.view.game;
 
+import com.googlecode.lanterna.TextColor;
+import g0803.bindingofshiba.gui.GUI;
+import g0803.bindingofshiba.model.game.elements.Player;
+
 public class PlayerView {
+    private Player player;
+
+    public PlayerView(Player player) {
+        this.player = player;
+    }
+
+    public void draw(GUI gui) {
+
+    }
 }
