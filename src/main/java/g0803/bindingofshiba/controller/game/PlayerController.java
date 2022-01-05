ac29065d235d12c63f13c0226833ac713ddd86a2
Guilherme Almeida <gui1612@sapo.pt>
2022-01-05 00:17:05 +0000

    feat: make PlayerController tests pass

--- src/main/java/g0803/bindingofshiba/controller/game/PlayerController.java
+++ src/main/java/g0803/bindingofshiba/controller/game/PlayerController.java
@@ -14,10 +14,21 @@ public class PlayerController {
     }
 
     public void movePlayer() {
-
+        player.setPosition(getNextPosition());
     }
 
     public Position getNextPosition() {
-        return new Position(0, 0);
+        Position playerPos = player.getPosition();
+
+        if (keyboard.isKeyPressed(Keyboard.Key.A)) {
+            return playerPos.getLeft();
+        } else if (keyboard.isKeyPressed(Keyboard.Key.W)) {
+            return playerPos.getUp();
+        } else if (keyboard.isKeyPressed(Keyboard.Key.S)) {
+            return playerPos.getDown();
+        } else if (keyboard.isKeyPressed(Keyboard.Key.D)) {
+            return playerPos.getRight();
+        }
+        return playerPos;
     }
 }
