46b90dddce5718f43889e28f44876fcf20087bf2
marineve17 <mariana.lobao2002@gmail.com>
2022-01-06 13:43:09 +0000

    feat: make menu view tests pass

--- src/main/java/g0803/bindingofshiba/view/menu/MenuView.java
+++ src/main/java/g0803/bindingofshiba/view/menu/MenuView.java
@@ -1,5 +1,7 @@
 package g0803.bindingofshiba.view.menu;
 
+import g0803.bindingofshiba.model.game.Position;
+import com.googlecode.lanterna.TextColor;
 import g0803.bindingofshiba.gui.GUI;
 import g0803.bindingofshiba.model.menu.Menu;
 
@@ -11,6 +13,16 @@ public class MenuView {
     }
 
     public void draw(GUI gui) {
+        TextColor foregroundColor = TextColor.Factory.fromString("#DAE3EA");
+        TextColor backgroundColor = TextColor.Factory.fromString("#034270");
+
+        gui.drawText(20, 2, menu.getTitle(), foregroundColor, backgroundColor);
+
+        Position firstPosition = new Position(4,4);
+        for (int i = 0; i < menu.getOptions().size(); i++) {
+            gui.drawText(firstPosition.getX(), firstPosition.getY(), "[" + Integer.toString(i+1) + "]" + menu.getOptions().get(i), foregroundColor, backgroundColor);
+            firstPosition = new Position(firstPosition.getX(), firstPosition.getY() + 2);
+        }
 
     }
 }
