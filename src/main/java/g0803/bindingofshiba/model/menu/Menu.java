cf6b049d66ca572dc10dd598ee5f42ab0e57b219
marineve17 <mariana.lobao2002@gmail.com>
2022-01-06 13:31:20 +0000

    feat: make menu tests pass

--- src/main/java/g0803/bindingofshiba/model/menu/Menu.java
+++ src/main/java/g0803/bindingofshiba/model/menu/Menu.java
@@ -1,13 +1,17 @@
 package g0803.bindingofshiba.model.menu;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
     private String title;
     private List<String> options;
+    private int choice;
 
     public Menu(String title, List<String> options) {
-
+        this.title = title;
+        this.options = new ArrayList<>(options);
+        this.choice = 0;
     }
 
     public String getTitle() {
@@ -19,7 +23,15 @@ public class Menu {
     }
 
     public void addOption(String option) {
+        this.options.add(option);
+    }
 
+    public void incrementChoice() {
+        this.choice++;
+    }
+
+    public void decrementChoice() {
+        this.choice--;
     }
 
 }
