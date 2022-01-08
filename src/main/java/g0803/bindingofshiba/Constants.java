4c4e932f5556bd6667afdf12fd293eedc4df815f
André Lima <me@limwa.pt>
2022-01-08 23:23:22 +0000

    refactor: accept suggestions
    Co-authored-by: Guilherme Almeida <me@gui1612.pt>

--- src/main/java/g0803/bindingofshiba/Constants.java
+++ src/main/java/g0803/bindingofshiba/Constants.java
@@ -6,4 +6,7 @@ public class Constants {
 
     public static final String SQUARE_FONT_LOCATION = "/fonts/square.ttf";
     public static final int SQUARE_FONT_SIZE = 10;
+
+    public static final int WIDTH = 150;
+    public static final int HEIGHT = 75;
 }
