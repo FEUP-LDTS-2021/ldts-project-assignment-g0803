46683598e15165c9e7d518e4d72e2b1d5c4516f7
Guilherme Almeida <gui1612@sapo.pt>
2022-01-09 00:12:19 +0000

    refactor: use generic view classes

--- src/main/java/g0803/bindingofshiba/view/game/PlayerView.java
+++ src/main/java/g0803/bindingofshiba/view/game/PlayerView.java
@@ -3,21 +3,22 @@ package g0803.bindingofshiba.view.game;
 import com.googlecode.lanterna.TextColor;
 import g0803.bindingofshiba.gui.GUI;
 import g0803.bindingofshiba.model.game.elements.Player;
+import g0803.bindingofshiba.view.View;
 
-public class PlayerView {
-    private Player player;
+public class PlayerView extends View<Player> {
 
-    public PlayerView(Player player) {
-        this.player = player;
+    public PlayerView(Player model) {
+        super(model);
     }
 
+    @Override
     public void draw(GUI gui) {
         TextColor foregroundColor = TextColor.Factory.fromString("#FF00FF");
         TextColor backgroundColor = TextColor.Factory.fromString("#444444");
 
         gui.drawText(
-                player.getPosition().getX(),
-                player.getPosition().getY(),
+                this.getModel().getPosition().getX(),
+                this.getModel().getPosition().getY(),
                 "X",
                 foregroundColor,
                 backgroundColor);
