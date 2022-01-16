a5be1bfa57ea4e8ec3ad6d904fc8c51330823f20
André Lima <me@limwa.pt>
2022-01-16 22:49:48 +0000

    test: add PlayerView tests

--- src/test/java/g0803/bindingofshiba/view/game/PlayerViewTest.java
+++ src/test/java/g0803/bindingofshiba/view/game/PlayerViewTest.java
@@ -1,14 +1,35 @@
 package g0803.bindingofshiba.view.game;
 
 import com.googlecode.lanterna.TextColor;
+import g0803.bindingofshiba.App;
+import g0803.bindingofshiba.bundles.Bundle;
 import g0803.bindingofshiba.gui.GUI;
+import g0803.bindingofshiba.math.Vec2D;
 import g0803.bindingofshiba.model.game.Position;
+import g0803.bindingofshiba.model.game.elements.Monster;
 import g0803.bindingofshiba.model.game.elements.Player;
+import g0803.bindingofshiba.textures.ITexture;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 public class PlayerViewTest {
-//    public Player getPlayer() {
-//        return new Player(new Position(5, 5), 0, 50, 20);
-//    }
+
+    @Test
+    public void blitsCorrectTexture() {
+        App app = Mockito.mock(App.class);
+        GUI gui = Mockito.mock(GUI.class);
+
+        Bundle<ITexture> textures = Mockito.mock(Bundle.class);
+        ITexture texture = Mockito.mock(ITexture.class);
+
+        Mockito.when(app.getTextures()).thenReturn(textures);
+        Mockito.when(textures.get("shiba")).thenReturn(texture);
+
+        Player player = new Player(new Vec2D(7, 3), 1, 20, 2);
+        PlayerView view = new PlayerView(player);
+
+        view.draw(app, gui, new Vec2D(4, -2));
+
+        Mockito.verify(gui).blit(11, 1, texture);
+    }
 }
