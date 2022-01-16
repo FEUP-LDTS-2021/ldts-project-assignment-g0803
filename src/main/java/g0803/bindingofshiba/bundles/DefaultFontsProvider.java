47ecaa1de8d06a8555e09932631c54506f5ef1b1
André Lima <me@limwa.pt>
2022-01-16 20:11:35 +0000

    feat: add bundle providers

---
+++ src/main/java/g0803/bindingofshiba/bundles/DefaultFontsProvider.java
@@ -0,0 +1,11 @@
+package g0803.bindingofshiba.bundles;
+
+import java.awt.*;
+
+public class DefaultFontsProvider implements BundleProvider<Font> {
+
+    @Override
+    public Bundle<Font> getBundle() {
+        throw new RuntimeException("Not implemented");
+    }
+}
---
+++ src/main/java/g0803/bindingofshiba/bundles/DefaultTexturesProvider.java
@@ -0,0 +1,11 @@
+package g0803.bindingofshiba.bundles;
+
+import g0803.bindingofshiba.textures.ITexture;
+
+public class DefaultTexturesProvider implements BundleProvider<ITexture> {
+
+    @Override
+    public Bundle<ITexture> getBundle() {
+        throw new RuntimeException("Not implemented");
+    }
+}
