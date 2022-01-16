7b9686372baed51d5a3c7790cc6ab26905e36377
André Lima <me@limwa.pt>
2022-01-16 22:54:33 +0000

    feat: make PlayerView tests pass

--- src/main/java/g0803/bindingofshiba/view/game/PlayerView.java
+++ src/main/java/g0803/bindingofshiba/view/game/PlayerView.java
@@ -1,9 +1,11 @@
 package g0803.bindingofshiba.view.game;
 
 import g0803.bindingofshiba.App;
+import g0803.bindingofshiba.bundles.Bundle;
 import g0803.bindingofshiba.gui.GUI;
 import g0803.bindingofshiba.math.Vec2D;
 import g0803.bindingofshiba.model.game.elements.Player;
+import g0803.bindingofshiba.textures.ITexture;
 import g0803.bindingofshiba.view.View;
 
 public class PlayerView extends View<Player> {
@@ -14,6 +16,9 @@ public class PlayerView extends View<Player> {
 
     @Override
     public void draw(App app, GUI gui, Vec2D offset) {
-        throw new RuntimeException("Not implemented");
+        Bundle<ITexture> textures = app.getTextures();
+        Vec2D position = getModel().getPosition().add(offset).round();
+
+        gui.blit((int) position.getX(), (int) position.getY(), textures.get("shiba"));
     }
 }
