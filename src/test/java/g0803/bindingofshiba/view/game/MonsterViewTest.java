fe4db00a8af6770cff4731a0cc8473d25589c877
André Lima <me@limwa.pt>
2022-01-16 22:49:21 +0000

    test: make tests for MonsterView

--- src/test/java/g0803/bindingofshiba/view/game/MonsterViewTest.java
+++ src/test/java/g0803/bindingofshiba/view/game/MonsterViewTest.java
@@ -1,12 +1,32 @@
 package g0803.bindingofshiba.view.game;
 
-import com.googlecode.lanterna.TextColor;
+import g0803.bindingofshiba.App;
+import g0803.bindingofshiba.bundles.Bundle;
 import g0803.bindingofshiba.gui.GUI;
-import g0803.bindingofshiba.model.game.Position;
+import g0803.bindingofshiba.math.Vec2D;
 import g0803.bindingofshiba.model.game.elements.Monster;
+import g0803.bindingofshiba.textures.ITexture;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 public class MonsterViewTest {
 
+    @Test
+    public void blitsNormalTexture() {
+        App app = Mockito.mock(App.class);
+        GUI gui = Mockito.mock(GUI.class);
+
+        Bundle<ITexture> textures = Mockito.mock(Bundle.class);
+        ITexture texture = Mockito.mock(ITexture.class);
+
+        Mockito.when(app.getTextures()).thenReturn(textures);
+        Mockito.when(textures.get("monster.normal")).thenReturn(texture);
+
+        Monster monster = new Monster(new Vec2D(10, 4), 20, 4);
+        MonsterView view = new MonsterView(monster);
+
+        view.draw(app, gui, new Vec2D(-1, 2));
+
+        Mockito.verify(gui).blit(9, 6, texture);
+    }
 }
