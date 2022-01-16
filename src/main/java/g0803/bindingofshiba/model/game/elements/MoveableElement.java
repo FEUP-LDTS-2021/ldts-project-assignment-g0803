9fba3da0e737c34140f923990a492ce79f0fd02f
André Lima <me@limwa.pt>
2022-01-16 15:44:20 +0000

    feat: make tests for MoveableElement pass

--- src/main/java/g0803/bindingofshiba/model/game/elements/MoveableElement.java
+++ src/main/java/g0803/bindingofshiba/model/game/elements/MoveableElement.java
@@ -4,28 +4,34 @@ import g0803.bindingofshiba.math.Vec2D;
 
 public class MoveableElement extends Element {
 
+    private Vec2D velocity = Vec2D.zero();
+    private Vec2D acceleration = Vec2D.zero();
+
     public MoveableElement(Vec2D position) {
         super(position);
-        throw new RuntimeException("Not implemented");
     }
 
     public Vec2D getVelocity() {
-        throw new RuntimeException("Not implemented");
+        return velocity;
     }
 
     public Vec2D getAcceleration() {
-        throw new RuntimeException("Not implemented");
+        return acceleration;
     }
 
     public void setVelocity(Vec2D velocity) {
-        throw new RuntimeException("Not implemented");
+        this.velocity = velocity;
     }
 
     public void setAcceleration(Vec2D acceleration) {
-        throw new RuntimeException("Not implemented");
+        this.acceleration = acceleration;
     }
 
     public void move(double dt) {
-        throw new RuntimeException("Not implemented");
+        Vec2D nextVelocity = this.velocity.add(this.acceleration.scale(dt));
+        Vec2D nextPosition = this.getPosition().add(nextVelocity.scale(dt));
+
+        this.setVelocity(nextVelocity);
+        this.setPosition(nextPosition);
     }
 }
