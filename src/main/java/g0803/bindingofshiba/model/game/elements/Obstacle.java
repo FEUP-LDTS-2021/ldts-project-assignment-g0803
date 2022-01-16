bce03a135b14b6af1ba71de050f18ffcbc6cc66b
Guilherme Almeida <gui1612@sapo.pt>
2022-01-16 23:22:37 +0000

    feat: add interfaces for Obstacle

--- src/main/java/g0803/bindingofshiba/model/game/elements/Obstacle.java
+++ src/main/java/g0803/bindingofshiba/model/game/elements/Obstacle.java
@@ -1,4 +1,14 @@
 package g0803.bindingofshiba.model.game.elements;
 
-public class Obstacle {
+import g0803.bindingofshiba.math.Vec2D;
+
+public class Obstacle extends Element {
+
+    public Obstacle(Vec2D position) {
+        super(position);
+    }
+
+    public Obstacle(int x, int y) {
+        super(x, y);
+    }
 }
