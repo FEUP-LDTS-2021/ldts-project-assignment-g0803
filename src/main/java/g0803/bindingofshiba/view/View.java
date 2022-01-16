2f0497d18efc8f5bfdf17d5bda425682385a0140
André Lima <me@limwa.pt>
2022-01-16 22:48:56 +0000

    feat: add offset to the draw method

--- src/main/java/g0803/bindingofshiba/view/View.java
+++ src/main/java/g0803/bindingofshiba/view/View.java
@@ -3,6 +3,7 @@ package g0803.bindingofshiba.view;
 import g0803.bindingofshiba.App;
 import g0803.bindingofshiba.bundles.Bundle;
 import g0803.bindingofshiba.gui.GUI;
+import g0803.bindingofshiba.math.Vec2D;
 import g0803.bindingofshiba.textures.ITexture;
 
 public abstract class View<T> {
@@ -17,5 +18,5 @@ public abstract class View<T> {
         return model;
     }
 
-    public abstract void draw(App app, GUI gui);
+    public abstract void draw(App app, GUI gui, Vec2D offset);
 }
--- src/main/java/g0803/bindingofshiba/view/game/PlayerView.java
+++ src/main/java/g0803/bindingofshiba/view/game/PlayerView.java
@@ -2,6 +2,7 @@ package g0803.bindingofshiba.view.game;
 
 import g0803.bindingofshiba.App;
 import g0803.bindingofshiba.gui.GUI;
+import g0803.bindingofshiba.math.Vec2D;
 import g0803.bindingofshiba.model.game.elements.Player;
 import g0803.bindingofshiba.view.View;
 
@@ -12,7 +13,7 @@ public class PlayerView extends View<Player> {
     }
 
     @Override
-    public void draw(App app, GUI gui) {
+    public void draw(App app, GUI gui, Vec2D offset) {
         throw new RuntimeException("Not implemented");
     }
 }
