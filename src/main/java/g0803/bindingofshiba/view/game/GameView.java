be919b89d36d03f3d327aad187d2530bc5f25b02
André Lima <me@limwa.pt>
2022-01-28 21:23:38 +0000

    feat: add factories to GameView

--- src/main/java/g0803/bindingofshiba/view/game/GameView.java
+++ src/main/java/g0803/bindingofshiba/view/game/GameView.java
@@ -1,17 +1,25 @@
 package g0803.bindingofshiba.view.game;
 
 import g0803.bindingofshiba.App;
+import g0803.bindingofshiba.factory.ViewFactory;
 import g0803.bindingofshiba.gui.GUI;
 import g0803.bindingofshiba.math.Vec2D;
 import g0803.bindingofshiba.model.game.Game;
+import g0803.bindingofshiba.model.game.elements.Monster;
+import g0803.bindingofshiba.model.game.elements.Player;
 import g0803.bindingofshiba.view.View;
+
 
 public class GameView extends View<Game> {
 
-    public GameView(Game model) {
+    public GameView(Game model, ViewFactory<Player> playerViewFactory, ViewFactory<Monster> monsterViewFactory) {
         super(model);
+
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
-    public void draw(App app, GUI gui, Vec2D offset) {}
+    public void draw(App app, GUI gui, Vec2D offset) {
+
+    }
 }
