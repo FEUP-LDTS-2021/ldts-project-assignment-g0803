248a5e37082b61a9b29433d945571f352a40860d
André Lima <me@limwa.pt>
2022-01-28 20:18:41 +0000

    feat: add monsters and player to game model

--- src/main/java/g0803/bindingofshiba/model/game/Game.java
+++ src/main/java/g0803/bindingofshiba/model/game/Game.java
@@ -1,3 +1,33 @@
 package g0803.bindingofshiba.model.game;
 
-public class Game {}
+import g0803.bindingofshiba.math.Vec2D;
+import g0803.bindingofshiba.model.game.elements.Monster;
+import g0803.bindingofshiba.model.game.elements.Player;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Game {
+
+    private final Player player;
+    private final List<Monster> monsters;
+
+    public Game() {
+        this.player = new Player(new Vec2D(10, 20), 0, 30, 5);
+        this.monsters = Arrays.asList(new Monster(new Vec2D(15, 40), 3, 1), new Monster(new Vec2D(10, 50), 5, 2));
+    }
+
+    public Game(Player player, List<Monster> monsters) {
+        this.player = player;
+        this.monsters = new ArrayList<>(monsters);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public List<Monster> getMonsters() {
+        return monsters;
+    }
+}
