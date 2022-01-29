2c100144f58a2766ef6eab2e6b2e36fb8213bacd
André Lima <me@limwa.pt>
2022-01-29 15:33:37 +0000

    test: add tests for monster controller

--- src/test/java/g0803/bindingofshiba/controller/game/MonsterControllerTest.java
+++ src/test/java/g0803/bindingofshiba/controller/game/MonsterControllerTest.java
@@ -1,7 +1,10 @@
 package g0803.bindingofshiba.controller.game;
 
 import g0803.bindingofshiba.App;
+import g0803.bindingofshiba.events.Event;
 import g0803.bindingofshiba.events.EventManager;
+import g0803.bindingofshiba.events.game.MonsterCollisionWithMonsterEvent;
+import g0803.bindingofshiba.events.game.PlayerCollisionWithMonsterEvent;
 import g0803.bindingofshiba.math.Vec2D;
 import g0803.bindingofshiba.model.game.Game;
 import g0803.bindingofshiba.model.game.elements.Monster;
@@ -52,5 +55,53 @@ public class MonsterControllerTest {
 
         Mockito.verify(monster).move(3);
         Mockito.verify(monster).setAcceleration(Mockito.argThat(vec -> vec.normalize().isSimilar(new Vec2D(4, 8).normalize())));
+    }
+
+    @Test
+    public void collisionWithMonster() {
+        Monster monster1 = Mockito.mock(Monster.class);
+        Monster monster2 = Mockito.mock(Monster.class);
+
+        Game game = Mockito.mock(Game.class);
+        EventManager manager = Mockito.mock(EventManager.class);
+
+        MonsterController controller = new MonsterController(game, manager);
+        Mockito.verify(manager).addObserver(controller);
+
+        MonsterCollisionWithMonsterEvent event = new MonsterCollisionWithMonsterEvent(3, game, monster1, monster2);
+
+        Mockito.when(monster1.getNextPosition(3)).thenReturn(new Vec2D(3, 4));
+        Mockito.when(monster2.getNextPosition(3)).thenReturn(new Vec2D(6, 7));
+        Mockito.when(monster1.getNextVelocity(3)).thenReturn(new Vec2D(3, 5));
+        Mockito.when(monster2.getNextVelocity(3)).thenReturn(new Vec2D(8, 9));
+
+        controller.onMonsterCollisionWithMonster(event);
+
+        Mockito.verify(monster1).setVelocity(Mockito.argThat(vec -> !vec.isSimilar(new Vec2D(3, 5))));
+        Mockito.verify(monster2).setVelocity(Mockito.argThat(vec -> !vec.isSimilar(new Vec2D(8, 9))));
+    }
+
+    @Test
+    public void collisionWithPlayer() {
+        Monster monster = Mockito.mock(Monster.class);
+        Player player = Mockito.mock(Player.class);
+
+        Game game = Mockito.mock(Game.class);
+        EventManager manager = Mockito.mock(EventManager.class);
+
+        MonsterController controller = new MonsterController(game, manager);
+        Mockito.verify(manager).addObserver(controller);
+
+        PlayerCollisionWithMonsterEvent event = new PlayerCollisionWithMonsterEvent(2, game, player, monster);
+
+        Mockito.when(monster.getNextPosition(2)).thenReturn(new Vec2D(3, -2));
+        Mockito.when(monster.getNextVelocity(2)).thenReturn(new Vec2D(-1, 0));
+
+        controller.onPlayerCollisionWithMonster(event);
+
+        Mockito.verify(monster).setVelocity(Vec2D.zero());
+        Mockito.verify(monster).setAcceleration(Vec2D.zero());
+
+        Mockito.verifyNoInteractions(player);
     }
 }
