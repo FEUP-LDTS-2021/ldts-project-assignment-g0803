d681fb6ebb2962fa78a72fdbe922e6aa8791a0e2
André Lima <me@limwa.pt>
2022-01-29 03:07:53 +0000

    feat: add bounding boxes provider

---
+++ src/main/java/g0803/bindingofshiba/bundles/DefaultBoundingBoxesProvider.java
@@ -0,0 +1,11 @@
+package g0803.bindingofshiba.bundles;
+
+import g0803.bindingofshiba.math.BoundingBox;
+
+public class DefaultBoundingBoxesProvider implements BundleProvider<BoundingBox> {
+
+    @Override
+    public Bundle<BoundingBox> getBundle() {
+        throw new RuntimeException("Not implemented");
+    }
+}
