110f5fb2d1cc3203f965e04f51f7c83da03393e2
André Lima <me@limwa.pt>
2022-01-29 00:19:04 +0000

    refactor: remove factory package

--- src/main/java/g0803/bindingofshiba/factory/ViewFactory.java
+++
@@ -1,8 +0,0 @@
-package g0803.bindingofshiba.factory;
-
-import g0803.bindingofshiba.view.View;
-
-public interface ViewFactory<T> {
-
-    View<T> create(T model);
-}
--- src/test/java/g0803/bindingofshiba/view/game/GameViewTest.java
+++ src/test/java/g0803/bindingofshiba/view/game/GameViewTest.java
@@ -3,7 +3,7 @@ package g0803.bindingofshiba.view.game;
 import g0803.bindingofshiba.App;
 import g0803.bindingofshiba.bundles.Bundle;
 import g0803.bindingofshiba.bundles.DefaultFontsProvider;
-import g0803.bindingofshiba.factory.ViewFactory;
+import g0803.bindingofshiba.view.ViewFactory;
 import g0803.bindingofshiba.gui.GUI;
 import g0803.bindingofshiba.math.Vec2D;
 import g0803.bindingofshiba.model.game.Game;
