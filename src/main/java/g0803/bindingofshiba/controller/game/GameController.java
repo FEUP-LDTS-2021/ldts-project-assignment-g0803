c52788fb9b15d89612b699a1c59c5d6731d20953
André Lima <me@limwa.pt>
2022-01-29 03:18:37 +0000

    feat: add events to controllers

--- src/main/java/g0803/bindingofshiba/controller/game/GameController.java
+++ src/main/java/g0803/bindingofshiba/controller/game/GameController.java
@@ -2,13 +2,14 @@ package g0803.bindingofshiba.controller.game;
 
 import g0803.bindingofshiba.App;
 import g0803.bindingofshiba.controller.Controller;
+import g0803.bindingofshiba.events.EventManager;
 import g0803.bindingofshiba.model.game.Game;
 import java.io.IOException;
 
 public class GameController extends Controller<Game> {
 
-    public GameController(Game model) {
-        super(model);
+    public GameController(Game model, EventManager eventManager) {
+        super(model, eventManager);
     }
 
     @Override
