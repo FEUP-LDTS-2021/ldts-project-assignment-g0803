57b9040ee5cfb0238f005b4eb80c3c9ec380b512
André Lima <me@limwa.pt>
2022-01-30 07:02:27 +0000

    feat: make tests for projectile view pass

--- src/main/java/g0803/bindingofshiba/view/game/ProjectileView.java
+++ src/main/java/g0803/bindingofshiba/view/game/ProjectileView.java
@@ -1,10 +1,12 @@
 package g0803.bindingofshiba.view.game;
 
 import g0803.bindingofshiba.App;
+import g0803.bindingofshiba.bundles.Bundle;
 import g0803.bindingofshiba.events.IEventManager;
 import g0803.bindingofshiba.gui.GUI;
 import g0803.bindingofshiba.math.Vec2D;
 import g0803.bindingofshiba.model.game.elements.Projectile;
+import g0803.bindingofshiba.textures.ITexture;
 import g0803.bindingofshiba.view.View;
 
 public class ProjectileView extends View<Projectile> {
@@ -15,6 +17,9 @@ public class ProjectileView extends View<Projectile> {
 
     @Override
     public void draw(App app, GUI gui, Vec2D offset) {
-        throw new RuntimeException("Not implemented");
+        Bundle<ITexture> textures = app.getTextures();
+        Vec2D position = getModel().getPosition().add(offset).round();
+
+        gui.blit((int) position.getX(), (int) position.getY(), textures.get("heart"));
     }
 }
