54f574fa4d034650f53e28d921dd0fb90683bed4
André Lima <me@limwa.pt>
2022-01-30 07:12:45 +0000

    feat: add data to events

--- src/main/java/g0803/bindingofshiba/events/game/ProjectileCollisionWithMonsterEvent.java
+++ src/main/java/g0803/bindingofshiba/events/game/ProjectileCollisionWithMonsterEvent.java
@@ -2,10 +2,26 @@ package g0803.bindingofshiba.events.game;
 
 import g0803.bindingofshiba.App;
 import g0803.bindingofshiba.events.Event;
+import g0803.bindingofshiba.model.game.elements.Monster;
+import g0803.bindingofshiba.model.game.elements.Projectile;
 
 public class ProjectileCollisionWithMonsterEvent extends Event {
 
-    public ProjectileCollisionWithMonsterEvent(double dt, App app) {
+    private final Projectile projectile;
+    private final Monster monster;
+
+    public ProjectileCollisionWithMonsterEvent(double dt, App app, Projectile projectile, Monster monster) {
         super(dt, app);
+
+        this.projectile = projectile;
+        this.monster = monster;
+    }
+
+    public Projectile getProjectile() {
+        return projectile;
+    }
+
+    public Monster getMonster() {
+        return monster;
     }
 }
--- src/main/java/g0803/bindingofshiba/events/game/ProjectileDestroyedEvent.java
+++ src/main/java/g0803/bindingofshiba/events/game/ProjectileDestroyedEvent.java
@@ -2,10 +2,19 @@ package g0803.bindingofshiba.events.game;
 
 import g0803.bindingofshiba.App;
 import g0803.bindingofshiba.events.Event;
+import g0803.bindingofshiba.model.game.elements.Projectile;
 
 public class ProjectileDestroyedEvent extends Event {
 
-    public ProjectileDestroyedEvent(double dt, App app) {
+    private final Projectile projectile;
+
+    public ProjectileDestroyedEvent(double dt, App app, Projectile projectile) {
         super(dt, app);
+
+        this.projectile = projectile;
+    }
+
+    public Projectile getProjectile() {
+        return projectile;
     }
 }
