10336a19b6512533b4e2bcd20fc1d298fa6be729
André Lima <me@limwa.pt>
2022-01-30 05:42:28 +0000

    feat: make tests for new controllers pass

--- src/main/java/g0803/bindingofshiba/controller/game/events/PlayerToMonsterCollisionEventsController.java
+++ src/main/java/g0803/bindingofshiba/controller/game/events/PlayerToMonsterCollisionEventsController.java
@@ -1,9 +1,17 @@
 package g0803.bindingofshiba.controller.game.events;
 
 import g0803.bindingofshiba.App;
+import g0803.bindingofshiba.bundles.Bundle;
 import g0803.bindingofshiba.controller.Controller;
 import g0803.bindingofshiba.events.IEventManager;
+import g0803.bindingofshiba.events.game.PlayerCollisionWithMonsterEvent;
+import g0803.bindingofshiba.math.BoundingBox;
 import g0803.bindingofshiba.model.game.Game;
+import g0803.bindingofshiba.model.game.elements.Monster;
+import g0803.bindingofshiba.model.game.elements.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlayerToMonsterCollisionEventsController extends Controller<Game> {
 
@@ -13,6 +21,24 @@ public class PlayerToMonsterCollisionEventsController extends Controller<Game> {
 
     @Override
     public void tick(App app, double dt) {
-        throw new RuntimeException("Not implemented");
+        Bundle<BoundingBox> boundingBoxes = app.getBoundingBoxes();
+        Player player = getModel().getPlayer();
+        List<Monster> monsters = getModel().getCurrentRoom().getMonsters();
+        ArrayList<PlayerCollisionWithMonsterEvent> eventsToDispatch = new ArrayList<>();
+
+        BoundingBox playerBox = boundingBoxes.get("shiba");
+        BoundingBox playerBoundingBox = playerBox.translate(player.getNextPosition(dt));
+
+        BoundingBox monsterBox = boundingBoxes.get("monster");
+        for (Monster monster : monsters) {
+            BoundingBox monsterBoundingBox = monsterBox.translate(monster.getNextPosition(dt));
+
+            if (playerBoundingBox.collides(monsterBoundingBox))
+                eventsToDispatch.add(
+                        new PlayerCollisionWithMonsterEvent(dt, app, player, monster));
+        }
+
+        for (PlayerCollisionWithMonsterEvent event : eventsToDispatch)
+            getEventManager().dispatchEvent(event);
     }
 }
--- src/main/java/g0803/bindingofshiba/controller/game/events/PlayerToWallsCollisionEventsController.java
+++ src/main/java/g0803/bindingofshiba/controller/game/events/PlayerToWallsCollisionEventsController.java
@@ -1,9 +1,13 @@
 package g0803.bindingofshiba.controller.game.events;
 
 import g0803.bindingofshiba.App;
+import g0803.bindingofshiba.bundles.Bundle;
 import g0803.bindingofshiba.controller.Controller;
 import g0803.bindingofshiba.events.IEventManager;
+import g0803.bindingofshiba.events.game.PlayerCollisionWithWallsEvent;
+import g0803.bindingofshiba.math.BoundingBox;
 import g0803.bindingofshiba.model.game.Game;
+import g0803.bindingofshiba.model.game.elements.Player;
 
 public class PlayerToWallsCollisionEventsController extends Controller<Game> {
 
@@ -13,6 +17,14 @@ public class PlayerToWallsCollisionEventsController extends Controller<Game> {
 
     @Override
     public void tick(App app, double dt) {
-        throw new RuntimeException("Not implemented");
+        Bundle<BoundingBox> boundingBoxes = app.getBoundingBoxes();
+        Player player = getModel().getPlayer();
+
+        BoundingBox playerBox = boundingBoxes.get("shiba");
+        BoundingBox roomBox = boundingBoxes.get("room");
+
+        BoundingBox playerBoundingBox = playerBox.translate(player.getNextPosition(dt));
+        if (!roomBox.contains(playerBoundingBox))
+            getEventManager().dispatchEvent(new PlayerCollisionWithWallsEvent(dt, app, player));
     }
 }
