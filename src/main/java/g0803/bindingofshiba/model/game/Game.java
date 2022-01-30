91824b54e88de9bdad1ea4cd680c47e232282951
André Lima <me@limwa.pt>
2022-01-30 05:47:29 +0000

    feat: switch to using Room on Game model

--- src/main/java/g0803/bindingofshiba/model/game/Game.java
+++ src/main/java/g0803/bindingofshiba/model/game/Game.java
@@ -1,34 +1,48 @@
 package g0803.bindingofshiba.model.game;
 
+import g0803.bindingofshiba.Constants;
 import g0803.bindingofshiba.math.Vec2D;
 import g0803.bindingofshiba.model.game.elements.Monster;
+import g0803.bindingofshiba.model.game.elements.Obstacle;
 import g0803.bindingofshiba.model.game.elements.Player;
-import java.util.ArrayList;
+import g0803.bindingofshiba.model.game.room.Door;
+import g0803.bindingofshiba.model.game.room.DoorPosition;
+import g0803.bindingofshiba.model.game.room.Room;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class Game {
 
     private final Player player;
-    private final List<Monster> monsters;
+    private Room currentRoom;
 
     public Game() {
-        this.player = new Player(new Vec2D(10, 20), 0, 30, 5);
-        this.monsters =
-                Arrays.asList(
-                        new Monster(new Vec2D(15, 40), 3, 1), new Monster(new Vec2D(10, 50), 5, 2));
+        this.player = new Player(new Vec2D(10, 20), 1, 30, 5);
+        this.currentRoom = new Room(Constants.ROOM_WIDTH, Constants.ROOM_HEIGHT, Arrays.asList(
+                new Monster(new Vec2D(15, 40), 3, 1), new Monster(new Vec2D(10, 50), 5, 2)), List.of(new Obstacle(30, 15)));
+
+        Room room2 = new Room(Constants.ROOM_WIDTH, Constants.ROOM_HEIGHT, List.of(), List.of());
+        Door door = new Door(currentRoom, DoorPosition.TOP, room2, DoorPosition.BOTTOM);
+        currentRoom.addDoor(door);
+        room2.addDoor(door);
+
     }
 
-    public Game(Player player, List<Monster> monsters) {
+    public Game(Player player, Room room) {
         this.player = player;
-        this.monsters = new ArrayList<>(monsters);
+        this.currentRoom = room;
     }
 
     public Player getPlayer() {
         return player;
     }
 
-    public List<Monster> getMonsters() {
-        return monsters;
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+
+    public void setCurrentRoom(Room currentRoom) {
+        this.currentRoom = currentRoom;
     }
 }
