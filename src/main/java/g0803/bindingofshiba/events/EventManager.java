bbd0d9be869f94affbc131f724836fc95193a76f
André Lima <me@limwa.pt>
2022-01-30 05:25:23 +0000

    feat: make new tests for EventManager pass

--- src/main/java/g0803/bindingofshiba/events/EventManager.java
+++ src/main/java/g0803/bindingofshiba/events/EventManager.java
@@ -1,25 +1,40 @@
 package g0803.bindingofshiba.events;
 
-import g0803.bindingofshiba.events.game.MonsterCollisionWithMonsterEvent;
-import g0803.bindingofshiba.events.game.PlayerCollisionWithMonsterEvent;
+import g0803.bindingofshiba.events.game.*;
+
+import java.lang.ref.WeakReference;
 import java.util.*;
 
 public class EventManager implements IEventManager {
 
-    private final Set<Observer> observers = new HashSet<>();
+    private final Set<WeakReference<Observer>> refs = new HashSet<>();
 
     @Override
     public void addObserver(Observer observer) {
-        observers.add(observer);
+        refs.add(new WeakReference<>(observer));
     }
 
     @Override
     public void removeObserver(Observer observer) {
-        observers.remove(observer);
+        refs.removeIf(ref -> ref.refersTo(observer));
     }
 
     @Override
     public Set<Observer> getObservers() {
+        Set<Observer> observers = new HashSet<>();
+
+        Iterator<WeakReference<Observer>> iterator = refs.iterator();
+        while (iterator.hasNext()) {
+            WeakReference<Observer> ref = iterator.next();
+
+            if (ref.refersTo(null)) {
+                iterator.remove();
+                continue;
+            }
+
+            observers.add(ref.get());
+        }
+
         return observers;
     }
 
@@ -28,12 +43,24 @@ public class EventManager implements IEventManager {
             listener.onPlayerCollisionWithMonster(e);
         } else if (event instanceof MonsterCollisionWithMonsterEvent e) {
             listener.onMonsterCollisionWithMonster(e);
+        } else if (event instanceof PlayerCollisionWithObstacleEvent e) {
+            listener.onPlayerCollisionWithObstacle(e);
+        } else if (event instanceof MonsterCollisionWithObstacleEvent e) {
+            listener.onMonsterCollisionWithObstacle(e);
+        } else if (event instanceof PlayerCollisionWithWallsEvent e) {
+            listener.onPlayerCollisionWithWalls(e);
+        } else if (event instanceof MonsterCollisionWithWallsEvent e) {
+            listener.onMonsterCollisionWithWalls(e);
+        } else if (event instanceof PlayerEnterDoorEvent e) {
+            listener.onPlayerEnterDoor(e);
+        } else if (event instanceof PlayerUnlockDoorEvent e) {
+            listener.onPlayerUnlockDoor(e);
         }
     }
 
     @Override
     public void dispatchEvent(Event event) {
-        for (Observer observer : observers) {
+        for (Observer observer : getObservers()) {
             dispatchEvent(event, observer);
         }
     }
