29ec6261c86594d47637ae9e8634c0f9103471f4
André Lima <me@limwa.pt>
2022-01-30 11:42:49 +0000

    feat: add finish screen

--- src/main/java/g0803/bindingofshiba/controller/game/GameController.java
+++ src/main/java/g0803/bindingofshiba/controller/game/GameController.java
@@ -4,11 +4,18 @@ import g0803.bindingofshiba.App;
 import g0803.bindingofshiba.controller.Controller;
 import g0803.bindingofshiba.controller.game.events.CollisionEventsController;
 import g0803.bindingofshiba.events.IEventManager;
+import g0803.bindingofshiba.events.Observer;
+import g0803.bindingofshiba.events.game.PlayerEnterDoorEvent;
 import g0803.bindingofshiba.model.game.Game;
+import g0803.bindingofshiba.model.game.room.Door;
+import g0803.bindingofshiba.model.game.room.LastRoom;
+import g0803.bindingofshiba.model.game.room.Room;
+import g0803.bindingofshiba.state.end.GameOverState;
+
 import java.util.Arrays;
 import java.util.List;
 
-public class GameController extends Controller<Game> {
+public class GameController extends Controller<Game> implements Observer {
 
     private final List<? extends Controller<?>> controllers;
 
@@ -22,6 +29,8 @@ public class GameController extends Controller<Game> {
                         new MonsterController(getModel(), getEventManager()),
                         new ProjectileController(getModel(), getEventManager()),
                         new RoomController(getModel(), getEventManager()));
+
+        getEventManager().addObserver(this);
     }
 
     public GameController(
@@ -34,5 +43,15 @@ public class GameController extends Controller<Game> {
     @Override
     public void tick(App app, double dt) {
         for (Controller<?> controller : controllers) controller.tick(app, dt);
+    }
+
+    @Override
+    public void onPlayerEnterDoor(PlayerEnterDoorEvent event) {
+        Room currentRoom = getModel().getCurrentRoom();
+        Door door = event.getDoor();
+
+        Room destination = door.getOtherRoom(currentRoom);
+        if (destination instanceof LastRoom)
+            event.getApp().setState(new GameOverState(true));
     }
 }
